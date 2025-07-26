371ea8911c1068b230a0a250646715134da31f39
Josh <josh.hua10@gmail.com>
2025-07-26 18:49:24 +1000

    Imported image for food object and made it a strawberry using textures and also implemented random position generating funciton for the food object

--- game.cpp
+++ game.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <cstdlib>  
 #include <ctime>
+#include <deque>
 #include "raylib.h"
 
 using namespace std;
@@ -15,13 +16,35 @@ Color blueGlow = {0, 200, 255, 255};
 int cellSize = 30;
 int cellCount = 25;
 
+//class for the actual snake, initializing it as 3 cells long
+
+
 //class for food objects
 class Food{
 public:
     Vector2 position = {6,9};
+    Texture2D texture;
+
+    //Loading actual image as the food object
+    Food(){
+        Image image = LoadImage("Graphics/food.png");
+        texture  = LoadTextureFromImage(image);
+        UnloadImage(image);
+        position = GenerateRandomPos();
+    }
+    //destructor for freeing memory after use
+    ~Food(){
+        UnloadTexture(texture);
+    }
 
     void Draw(){
-        DrawRectangle(position.x*cellSize,position.y*cellSize,cellSize,cellSize,blueGlow);
+        DrawTexture(texture, position.x*cellSize, position.y*cellSize, WHITE);
+    }
+    //generate a random position for each food using these Raylib functions
+    Vector2 GenerateRandomPos(){
+        float x = GetRandomValue(0,cellCount-1);
+        float y = GetRandomValue(0,cellCount-1);
+        return Vector2{x,y};
     }
 
 };
@@ -35,6 +58,9 @@ int main() {
 
     //create food object to use in game loop
     Food food = Food();
+    Food food1 = Food();
+    Food food2 = Food();
+    
 
     // Main game loop, if user presses escape or x then ends game loop.
     while(WindowShouldClose()==false){
@@ -43,6 +69,8 @@ int main() {
         //drawing the background
         ClearBackground(purple);
         food.Draw();
+        food1.Draw();
+        food2.Draw();
 
         EndDrawing();
     }
